a300f04e0b81e73c38c1a9c546f0958addf914a7
Sergio Toledo Recio <sertorre18@hotmail.com>
2019-01-24 09:02:13 +0100

    Añadida Animación

--- ControlCustomizado/ControlCustomizado/ControlPlanetario.cs
+++ ControlCustomizado/ControlCustomizado/ControlPlanetario.cs
@@ -9,6 +9,7 @@ using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Shapes;
 
 namespace ControlCustomizado
@@ -30,44 +31,82 @@ namespace ControlCustomizado
         public static readonly DependencyProperty ItemsSourceProperty =
             DependencyProperty.Register("ItemsSource", typeof(ObservableCollection<Planeta>), typeof(ControlPlanetario), new PropertyMetadata(null));
 
-
+        public int TiempoGiroAnimacion
+        {
+            get { return (int)GetValue(TiempoGiroAnimacionProperty); }
+            set { SetValue(TiempoGiroAnimacionProperty, value); }
+        }
+        // Using a DependencyProperty as the backing store for TiempoGiroAnimacion.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TiempoGiroAnimacionProperty =
+            DependencyProperty.Register("TiempoGiroAnimacion", typeof(int), typeof(ControlPlanetario), new PropertyMetadata(15, null));
 
         protected override void OnApplyTemplate()
         {
             base.OnApplyTemplate();
 
             Canvas CanvasDibujo = (Canvas)GetTemplateChild("CanvasPlanetario");
-            Canvas.SetTop(CanvasDibujo, 500);
-            Canvas.SetLeft(CanvasDibujo, 500);
-
-            CanvasDibujo.Height = 500;
+            Ellipse Sol = (Ellipse)GetTemplateChild("Sol");
+            double Multiplicador = CalcularMultiplicador();
             CanvasDibujo.Children.Clear();
 
-            Ellipse planeta1 = new Ellipse();
-            Ellipse planeta2 = new Ellipse();
-            Ellipse planeta3 = new Ellipse();
+            foreach (Planeta item in ItemsSource)
+            {
+                Ellipse planeta = new Ellipse();
 
-            planeta1.Height = 20;
-            planeta1.Width = 20;
-            planeta2.Height = 20;
-            planeta2.Width = 20;
-            planeta3.Height = 20;
-            planeta3.Width = 20;
+                planeta.Height = item.Diametro * Multiplicador;
+                planeta.Width = item.Diametro * Multiplicador;
 
-            planeta1.Fill = new SolidColorBrush(Colors.Red);
-            planeta2.Fill = new SolidColorBrush(Colors.Red);
-            planeta3.Fill = new SolidColorBrush(Colors.Red);
+                planeta.Fill = new SolidColorBrush(Colors.Red);
+                CanvasDibujo.Children.Add(planeta);
 
-            CanvasDibujo.Children.Add(planeta1);
-            CanvasDibujo.Children.Add(planeta2);
-            CanvasDibujo.Children.Add(planeta3);
+                Canvas.SetLeft(planeta, (this.Width / 2) - (item.DistanciaSol * Multiplicador));
+                Canvas.SetTop(planeta, (this.Height / 2) - (item.Diametro * Multiplicador / 2));
 
-            //Canvas.SetLeft(planeta, 250);
-            Canvas.SetTop(planeta1, Canvas.GetTop(CanvasDibujo) / 2);
-            Canvas.SetTop(planeta2, Canvas.GetTop(CanvasDibujo) / 2);
-            Canvas.SetLeft(planeta2, 75);
-            Canvas.SetTop(planeta3, Canvas.GetTop(CanvasDibujo) / 2);
-            Canvas.SetLeft(planeta3, 150);
+            }
+            AnimacionPlanetas(CanvasDibujo, Sol);
+        }
+
+        private double CalcularMultiplicador()
+        {
+            double MayorDistancia = 0;
+            foreach (Planeta item in ItemsSource)
+            {
+                if (MayorDistancia < item.DistanciaSol)
+                {
+                    MayorDistancia = item.DistanciaSol;
+                }
+            }
+
+            return (this.Width / 2) / MayorDistancia;
+        }
+
+        private void AnimacionPlanetas(Canvas CanvasPlanetas, Ellipse Sol)
+        {
+            Storyboard storyboard = new Storyboard();
+            storyboard.RepeatBehavior = RepeatBehavior.Forever;
+
+            for (int i = 0; i < CanvasPlanetas.Children.Count; i++)
+            {
+                DoubleAnimation rotateAnimation = new DoubleAnimation()
+                {
+                    From = 0,
+                    To = 360,
+                    Duration = new Duration(TimeSpan.FromSeconds(TiempoGiroAnimacion * (((this.Width / 2) - (Canvas.GetLeft(CanvasPlanetas.Children[i])))) / (this.Width / 2))),
+                    RepeatBehavior = RepeatBehavior.Forever
+                };
+
+
+                RotateTransform Rotar = new RotateTransform();
+                Rotar.CenterX = (this.Width / 2) - (Canvas.GetLeft(CanvasPlanetas.Children[i]));
+                CanvasPlanetas.Children[i].RenderTransform = Rotar;
+
+                Storyboard.SetTarget(rotateAnimation, CanvasPlanetas.Children[i]);
+                Storyboard.SetTargetProperty(rotateAnimation, "(UIElement.RenderTransform).(RotateTransform.Angle)");
+
+                storyboard.Children.Add(rotateAnimation);
+
+            }
+            storyboard.Begin();
         }
     }
 }
