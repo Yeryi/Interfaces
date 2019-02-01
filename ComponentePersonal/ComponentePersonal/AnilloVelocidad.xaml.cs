84897d4385c058ba03f08a0454aa7cd277840de5
Sergio Toledo Recio <sertorre18@hotmail.com>
2019-02-01 09:03:05 +0100

    Añadida animación y control digital de velocidad

--- ComponentePersonal/ComponentePersonal/AnilloVelocidad.xaml.cs
+++ ComponentePersonal/ComponentePersonal/AnilloVelocidad.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -19,15 +20,44 @@ namespace ComponentePersonal
 {
     public sealed partial class AnilloVelocidad : UserControl
     {
-        public int valor = 0;
+        //public int valor = -90;
         public AnilloVelocidad()
         {
             this.InitializeComponent();
+            valor = -90;
         }
 
         private void AumentarVelocidad(object sender, RoutedEventArgs e)
         {
-            valor++;
+            if (valor < 90)
+            {
+                Velocidad.Text = (valor + 90).ToString();
+                valor++;
+            }
+            
         }
+
+        private async void RepeatButton_LostFocus(object sender, RoutedEventArgs e)
+        {
+            while (valor >= -90)
+            {
+                Velocidad.Text = (valor + 90).ToString();
+                await Task.Delay(50);
+                valor--;
+            }
+        }
+
+
+        public int valor
+        {
+            get { return (int)GetValue(valorProperty); }
+            set { SetValue(valorProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for valor.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty valorProperty =
+            DependencyProperty.Register("valor", typeof(int), typeof(AnilloVelocidad), new PropertyMetadata(0));
+
+
     }
 }
